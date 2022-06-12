503c9de7b0358dd42c488fb560e2d4b6ad1bdb8b
Diogo-V <diogo.venancio00@gmail.com>
2022-06-13 00:02:37 +0100

    feat: add temporary landing page

--- ivmdatabase/settings.py
+++ ivmdatabase/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+h^av9juykm6hafb46e3w(!7s$9$875!cw3lb+xy8f&c_&pp3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'ivmdatabase.herokuapp.com']
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
+    'ivm'
 ]
 
 MIDDLEWARE = [
