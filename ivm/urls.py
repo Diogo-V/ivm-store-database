c1cd1111f06c4babfa7e71bce9a8bb72bbf23a43
Diogo-V <diogo.venancio00@gmail.com>
2022-06-17 10:19:04 +0100

    feat: add major routes

--- ivm/urls.py
+++ ivm/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.landing_page)
+    path('', views.landing_page_view),
+    path('retailer-more-replaces/', views.retailer_with_most_replaces_view),
+    path('retailers-simple-categories/', views.retailers_with_simple_categories_view),
+    path('products-never-replaced/', views.products_never_replaced_view),
+    path('products-replaced-same-retailer/', views.products_replaced_by_same_retailer_view),
 ]
--- ivm/views.py
+++ ivm/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
 
-def landing_page(request):
-    return render(request, 'index.html', {'name': 'Diogo'})
+def landing_page_view(request):
+    return render(request, 'index.html')
+
+
+def retailer_with_most_replaces_view(request):
+    return render(request, 'pages/retailer-most-replaces.html')
+
+
+def retailers_with_simple_categories_view(request):
+    return render(request, 'pages/retailers-simple-categories.html')
+
+
+def products_never_replaced_view(request):
+    return render(request, 'pages/products-never-replaced.html')
+
+
+def products_replaced_by_same_retailer_view(request):
+    return render(request, 'pages/products-replaced-same-retailer.html')
