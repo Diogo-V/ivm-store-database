fd0173fff3757d195d597fa75f8b33aa8efda589
Diogo-V <diogo.venancio00@gmail.com>
2022-06-17 13:08:22 +0100

    refactor: update validation

--- ivm/forms.py
+++ ivm/forms.py
@@ -2,9 +2,17 @@ from django import forms
 
 
 class RawDateSpanQuery(forms.Form):
-    start_date = forms.DateField()
-    end_date = forms.DateField()
+    start_date = forms.DateField(label='Data de começo')
+    end_date = forms.DateField(label='Data de fim')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        if start_date and end_date and start_date > end_date:  # Check if start_date is bigger and end_date
+            raise forms.ValidationError("A data de começo é depois da de fim")
 
 
 class RawDistrictQuery(forms.Form):
-    district = forms.CharField()
+    district = forms.CharField(label='Distrito')
